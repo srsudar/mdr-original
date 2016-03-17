8dabd118365529cd8cc8b7ac111c40f0b5da74e8
Sam Sudar <sudar.sam@gmail.com>
2016-03-17 14:38:10 -0700

    add JSON keys for pregnancy

--- app/src/main/java/org/mamasdelrio/android/util/JsonKeys.java
+++ app/src/main/java/org/mamasdelrio/android/util/JsonKeys.java
@@ -19,4 +19,18 @@ public class JsonKeys {
     public static final String BIRTH_DAY = "bd";
   }
 
+  /** Keys for the Pregnancies forms. */
+  public static class Pregnancies {
+    public static final String HAS_DNI = "h";
+    public static final String DNI = "d";
+    public static final String NAMES = "n";
+    public static final String BIRTH_YEAR = "by";
+    public static final String BIRTH_MONTH = "bm";
+    public static final String BIRTH_DAY = "bd";
+    public static final String PERIOD_KNOWN = "p";
+    public static final String PERIOD_DATE = "pd";
+    public static final String TAKE_CONTROLS = "c";
+    public static final String CONTROL_MONTH = "cm";
+  }
+
 }

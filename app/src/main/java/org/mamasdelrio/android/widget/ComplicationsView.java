b377727d80cce5f512e34b88da662a0c0b2986fe
Sam Sudar <sudar.sam@gmail.com>
2016-03-17 21:14:38 -0700

    move outcome complications to custom widget

---
+++ app/src/main/java/org/mamasdelrio/android/widget/ComplicationsView.java
@@ -0,0 +1,52 @@
+package org.mamasdelrio.android.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+import org.mamasdelrio.android.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by sudars on 3/17/16.
+ */
+public class ComplicationsView extends LinearLayout {
+  @Bind(R.id.outcome_complication_baby_state) SelectOneView babyState;
+  @Bind(R.id.outcome_complication_mother_state) SelectOneView motherState;
+
+  public ComplicationsView(Context context) {
+    super(context);
+    init();
+  }
+
+  public ComplicationsView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    init();
+  }
+
+  public ComplicationsView(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    init();
+  }
+
+  private void init() {
+    LayoutInflater.from(getContext()).inflate(R.layout.widget_complications,
+        this, true);
+    ButterKnife.bind(this);
+    babyState.initializeView(R.array.complications_baby_labels,
+        R.array.complications_baby_values);
+    motherState.initializeView(R.array.complications_mother_labels,
+        R.array.complications_mother_values);
+  }
+
+  public SelectOneView getBabyState() {
+    return babyState;
+  }
+
+  public SelectOneView getMotherState() {
+    return motherState;
+  }
+}
---
+++ app/src/test/java/org/mamasdelrio/android/widget/ComplicationsViewTest.java
@@ -0,0 +1,48 @@
+package org.mamasdelrio.android.widget;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mamasdelrio.android.BuildConfig;
+import org.mamasdelrio.android.DoAlarmActivity;
+import org.mamasdelrio.android.R;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+
+import static org.assertj.android.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests for {@link ComplicationsView}.
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class ComplicationsViewTest {
+  ComplicationsView view;
+  String[] babyOptions;
+  String[] motherOptions;
+
+  @Before
+  public void before() {
+    view = new ComplicationsView(RuntimeEnvironment.application);
+    babyOptions = RuntimeEnvironment.application.getResources().getStringArray(
+        R.array.complications_baby_values);
+    motherOptions = RuntimeEnvironment.application.getResources()
+        .getStringArray(R.array.complications_mother_values);
+  }
+
+  @Test
+  public void babyStateHasCorrectNumberOfOptions() {
+    assertThat(view.getBabyState().getSpinner()).hasCount(babyOptions.length);
+  }
+
+  @Test
+  public void motherStateHasCorrectNumberOfOptions() {
+    assertThat(view.getMotherState().getSpinner()).hasCount(
+        motherOptions.length);
+  }
+}

0be1b896b2a5960fee9fa14b8d8d4436b5cb8c7d
Sam Sudar <sudar.sam@gmail.com>
2016-03-17 20:32:13 -0700

    start adding content for outcome views

--- app/src/main/java/org/mamasdelrio/android/DoOutcomeActivity.java
+++ app/src/main/java/org/mamasdelrio/android/DoOutcomeActivity.java
@@ -2,8 +2,42 @@ package org.mamasdelrio.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import org.mamasdelrio.android.widget.SelectOneView;
+
+import butterknife.Bind;
 
 public class DoOutcomeActivity extends AppCompatActivity {
+  // Choose outcome
+  @Bind(R.id.outcome_select_outcome) RadioGroup selectOutcome;
+  @Bind(R.id.outcome_outcome_complications) RadioButton chooseComplications;
+  @Bind(R.id.outcome_outcome_abortion) RadioButton chooseAbortion;
+  @Bind(R.id.outcome_outcome_motherdeath) RadioButton chooseMotherDeath;
+  @Bind(R.id.outcome_outcome_babydeath) RadioButton chooseBabyDeath;
+
+  // Containers
+  @Bind(R.id.outcome_complications_container)
+  LinearLayout complicationsContainer;
+  @Bind(R.id.outcome_abortion_container) LinearLayout abortionContainer;
+  @Bind(R.id.outcome_maternaldeath_container)
+  LinearLayout maternalDeathContainer;
+  @Bind(R.id.outcome_babydeath_container) LinearLayout babyDeathContainer;
+
+  // Complications
+  @Bind(R.id.outcome_complication_baby_state) SelectOneView babyState;
+  @Bind(R.id.outcome_complication_mother_state) SelectOneView motherState;
+
+  // Abortion
+  @Bind(R.id.outcome_abortion_motherdni) EditText abortionDni;
+  @Bind(R.id.outcome_abortion_date) DatePicker abortionDate;
+
+  // Maternal death
+  @Bind(R.id.outcome_maternaldeath_date) DatePicker maternalDeathDate;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/test/java/org/mamasdelrio/android/DoOutcomeActivityTest.java
@@ -0,0 +1,18 @@
+package org.mamasdelrio.android;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.assertj.android.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests for {@link DoAlarmActivity}.
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class DoOutcomeActivityTest {
+}
